a61af8960b07f70f582e1fec37c8c5de4bfb72e6
Karoliina Hautala <karoliina.hautala@myy.haaga-helia.fi>
2019-06-18 13:34:00 +0300

    modifications

--- app/src/main/java/com/example/hpquiz/MainActivity.kt
+++ app/src/main/java/com/example/hpquiz/MainActivity.kt
@@ -3,24 +3,23 @@ package com.example.hpquiz
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         val buttonStartQuiz = findViewById<Button>(R.id.button_start_quiz)
-        buttonStartQuiz.setOnClickListener(View.OnClickListener { startQuiz() })
+        buttonStartQuiz.setOnClickListener{
+            val intent = Intent(this@MainActivity, QuizActivity::class.java)
+            startActivity(intent)
+        }
     }
 
-    private fun startQuiz() {
-        val intent = Intent(this@MainActivity, QuizActivity::class.java)
-        startActivity(intent)
-    }
 }
 
 
